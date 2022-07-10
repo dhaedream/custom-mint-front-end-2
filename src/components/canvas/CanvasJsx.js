4ab127142db50969656d9a3e3b529e740bd29e08
Dhaesha M <dhaesha1@gmail.com>
2022-07-10 02:22:13 -0400

    removed the code i commented out. I got stuck and went back to when the antenna was working to get the rest of the model to transform

--- src/components/canvas/CanvasJsx.js
+++ src/components/canvas/CanvasJsx.js
@@ -1,31 +1,3 @@
-// import React from "react";
-// import { Suspense, useRef, useState } from "react";
-// import { Canvas } from "@react-three/fiber";
-// import Alien from "../alien/Alien";
-// import { OrbitControls } from "@react-three/drei";
-
-// const CanvasJsx = ({ ...props }) => {
-//   console.log(props.aura);
-//   return (
-//     <Canvas>
-//       <Suspense fallback={null}>
-//         <ambientLight />
-//         <spotLight
-//           intensity={0.9}
-//           angle={0.1}
-//           penumbra={1}
-//           position={[10, 15, 10]}
-//           castShadow
-//         />
-//         <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-//         <Alien auraColor={props.aura} hex={props.hex} />
-//       </Suspense>
-//     </Canvas>
-//   );
-// };
-
-// export default CanvasJsx;
-
 import React from "react";
 import { Suspense, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
--- src/components/selector/Selector.jsx
+++ src/components/selector/Selector.jsx
@@ -1,122 +1,3 @@
-// import React, { useState } from "react";
-// import "./selector.css";
-// import antennaImg from "../../assets/icon-ant.png";
-// import eyesImg from "../../assets/icon-eye.png";
-// import faceImg from "../../assets/icon-face.png";
-// import bodyImg from "../../assets/icon-body.png";
-// import auraImg from "../../assets/icon-lightn.png";
-
-// const Selector = ({ ...props }) => {
-//   //   console.log({ ...props.customColor.aura });
-//   //   console.log({ ...props.customColor });
-
-//   return (
-//     <div className="selector">
-//       <h2>Customize:</h2>
-//       <div className="selector-container">
-//         <div className="selector-wrap">
-//           <div className="selector-object">
-//             <div className="selector-custom-container">
-//               <div className="selector-custom-wrap">
-//                 <img
-//                   className="selector-img-icon"
-//                   src={auraImg}
-//                   alt="aura-icon"
-//                 />
-//                 <input
-//                   type="color"
-//                   id="aura"
-//                   name="aura"
-//                   defaultValue={props.aura}
-//                   className="selector-input"
-//                   onChange={(e) => props.setAuraColors.aura(e.target.value)}
-//                   //   onChange={(e) => props.setCustomColors}
-//                 />
-//                 <label
-//                   for="aura"
-//                   className="selector-label.aura(e.target.value)"
-//                 >
-//                   Aura
-//                 </label>
-//               </div>
-//               <div className="selector-custom-wrap">
-//                 <img
-//                   className="selector-img-icon"
-//                   src={antennaImg}
-//                   alt="ant-icon"
-//                 />
-
-//                 <input
-//                   type="color"
-//                   id="antenna"
-//                   name="antenna"
-//                   className="selector-input"
-//                   //   value={props.antenna}
-//                   //   onChange={(e) => props.setAntenna(e.target.value)}
-//                 />
-//                 <label for="antenna" className="selector-label">
-//                   Antenna
-//                 </label>
-//               </div>
-//               <div className="selector-custom-wrap">
-//                 <img
-//                   className="selector-img-icon"
-//                   src={eyesImg}
-//                   alt="eyes-icon"
-//                 />
-//                 <input
-//                   type="color"
-//                   id="hair"
-//                   name="hair"
-//                   className="selector-input"
-//                   value={props.hex}
-//                 />
-//                 <label for="hair" className="selector-label">
-//                   Hair
-//                 </label>
-//               </div>
-//               <div className="selector-custom-wrap">
-//                 <img
-//                   className="selector-img-icon"
-//                   src={faceImg}
-//                   alt="face-icon"
-//                 />
-//                 <input
-//                   type="color"
-//                   id="face"
-//                   name="face"
-//                   className="selector-input"
-//                 />
-//                 <label for="face" className="selector-label">
-//                   Face
-//                 </label>
-//               </div>
-//               <div className="selector-custom-wrap">
-//                 <img
-//                   className="selector-img-icon"
-//                   src={bodyImg}
-//                   alt="body-icon"
-//                 />
-//                 <input
-//                   type="color"
-//                   id="body"
-//                   name="body"
-//                   className="selector-input"
-//                 />
-//                 <label for="face" className="selector-label">
-//                   Body
-//                 </label>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Selector;
-
 import React, { useState } from "react";
 import "./selector.css";
 import antennaImg from "../../assets/icon-ant.png";
@@ -127,23 +8,7 @@ import auraImg from "../../assets/icon-lightn.png";
 
 const Selector = ({ ...props }) => {
   console.log({ ...props });
-  //   const [customColors, setCustomColors] = useState({
-  //     aura: "#32a852",
-  //     antenna: "#32a852",
-  //     hair: "#32a852",
-  //     face: "#32a852",
-  //     body: "#32a852",
-  //     hex: "ffffff",
-  //   });
-  //   const [auraState, setAuraState] = useState("#ffffff");
 
-  //   const [aura, setAura] = useState("#32a852");
-  //   console.log(aura, setAura);
-  // const setAura;
-  // const [antenna, setAntenna ] = useState('#ffffff')
-  // const [hair, setHair ] = useState('#ffffff')
-  // const [face, setFacea ] = useState('#ffffff')
-  // const [body, setBody ] = useState('#ffffff')
   return (
     <div className="selector">
       <h2>Customize:</h2>
@@ -244,10 +109,6 @@ const Selector = ({ ...props }) => {
               </div>
             </div>
           </div>
-          {/* <div className="selector-custom-wrap">
-            <label for="face">Face</label>
-            <input type="color" id="face" name="face" />
-          </div> */}
         </div>
       </div>
     </div>
